508680d13c5c63da867a9c1aa7b8dd88604c13e2
Cristina Severin <cristina.severin@daimler.com>
2020-09-08 11:06:13 +0200

    Test 08092020

---
+++ build/precache-manifest.2bb19c36d50812fc1b3542569d2285cd.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b431dc02a3c61e36feab6c09e107de64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3aca5192ae08dbd7872",
+    "url": "/static/css/2.e1bc82ae.chunk.css"
+  },
+  {
+    "revision": "1024d1b86f2a1a3f49e7",
+    "url": "/static/css/main.fefda5c7.chunk.css"
+  },
+  {
+    "revision": "d3aca5192ae08dbd7872",
+    "url": "/static/js/2.276b24f2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.276b24f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1024d1b86f2a1a3f49e7",
+    "url": "/static/js/main.212c51e2.chunk.js"
+  },
+  {
+    "revision": "3bf1f5f6b5354392e081",
+    "url": "/static/js/runtime-main.3c8f5d11.js"
+  },
+  {
+    "revision": "9d6fbd2c02a7b3c007566ab2a50965f4",
+    "url": "/static/media/DaimlerCAC-Regular.9d6fbd2c.ttf"
+  },
+  {
+    "revision": "fb54a95624d7e03318f69750de640e9c",
+    "url": "/static/media/DaimlerCAC-Regular.fb54a956.woff"
+  },
+  {
+    "revision": "73167a2e0551b530b156c8c58acaf492",
+    "url": "/static/media/Daimler_Iconset-Basic_03.73167a2e.ttf"
+  },
+  {
+    "revision": "cc851de888dda68df422c4cc181cab9e",
+    "url": "/static/media/Daimler_Iconset-Basic_03.cc851de8.eot"
+  },
+  {
+    "revision": "f333fd2f32461d5454809d26032d4380",
+    "url": "/static/media/Daimler_Iconset-Basic_03.f333fd2f.woff"
+  },
+  {
+    "revision": "1422b984d6f911e93cda51230b04ba3b",
+    "url": "/static/media/Daimler_Iconset-Filetypes-Logos_01.1422b984.eot"
+  },
+  {
+    "revision": "48880d8211f7ba9b067f568b74161d41",
+    "url": "/static/media/Daimler_Iconset-Filetypes-Logos_01.48880d82.ttf"
+  },
+  {
+    "revision": "e96b81e3e8bbf706250bb512e2c26d1e",
+    "url": "/static/media/Daimler_Iconset-Filetypes-Logos_01.e96b81e3.woff"
+  },
+  {
+    "revision": "1670a0f75aadc1f895d54fa9028b2f1f",
+    "url": "/static/media/Daimler_Iconset-Interaction_01.1670a0f7.ttf"
+  },
+  {
+    "revision": "59325a332582680eade2232d61fc5161",
+    "url": "/static/media/Daimler_Iconset-Interaction_01.59325a33.eot"
+  },
+  {
+    "revision": "63f67680843f90a387d122adcfc8b298",
+    "url": "/static/media/Picture1.63f67680.jpg"
+  },
+  {
+    "revision": "9a2931180d6b1dc7b33052657eef554b",
+    "url": "/static/media/Segoe_UI.9a293118.woff"
+  },
+  {
+    "revision": "5c7d5421510ce2e60ba715e3d3a45170",
+    "url": "/static/media/daimler-cs-light-ttf.5c7d5421.ttf"
+  },
+  {
+    "revision": "e2e3cf6ee47a2669c74cc7658a109c1b",
+    "url": "/static/media/daimler-cs-light-woff.e2e3cf6e.woff"
+  },
+  {
+    "revision": "7e8d54e2aaae0c086cf26a145b033f5d",
+    "url": "/static/media/daimler_cs_bold_ttf.7e8d54e2.ttf"
+  },
+  {
+    "revision": "73020e191382a6ee08383f276ea3cd27",
+    "url": "/static/media/daimler_cs_bold_woff.73020e19.woff"
+  },
+  {
+    "revision": "402e62caa54b3f9d7c2c6d651182beb8",
+    "url": "/static/media/daimler_cs_demi_eot.402e62ca.eot"
+  },
+  {
+    "revision": "198d6016d312caf36dd77e761a025175",
+    "url": "/static/media/daimler_cs_demi_ttf.198d6016.ttf"
+  },
+  {
+    "revision": "5aa9ff27357f236b6a2e6ec21fe54be9",
+    "url": "/static/media/daimler_cs_demi_woff.5aa9ff27.woff"
+  },
+  {
+    "revision": "a3b895af54775f794d5d215e3cb18c61",
+    "url": "/static/media/daimler_cs_regular_eot.a3b895af.eot"
+  },
+  {
+    "revision": "48ebd7a568dc371045b245dc31f012cd",
+    "url": "/static/media/daimler_cs_regular_italic_eot.48ebd7a5.eot"
+  },
+  {
+    "revision": "3d6d84bf7ad684592f52f91b41301d6c",
+    "url": "/static/media/daimler_cs_regular_italic_ttf.3d6d84bf.ttf"
+  },
+  {
+    "revision": "ccfc9a976ce5fa982a29bfe1343423e7",
+    "url": "/static/media/daimler_cs_regular_italic_woff.ccfc9a97.woff"
+  },
+  {
+    "revision": "e63c51684446b7bd1db4b80c8f095e38",
+    "url": "/static/media/daimler_cs_regular_ttf.e63c5168.ttf"
+  },
+  {
+    "revision": "e0cf8aae9890211e5a7bdfea289b44b3",
+    "url": "/static/media/daimler_cs_regular_woff.e0cf8aae.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
